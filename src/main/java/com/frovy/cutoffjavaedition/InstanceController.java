f87ab51f5c77eef3ca97c000cdcdea157e2fb815
Frovu <sfrovis@gmail.com>
2021-05-27 19:58:32 +0300

    fix: date format

--- src/main/java/com/frovy/cutoffjavaedition/InstanceController.java
+++ src/main/java/com/frovy/cutoffjavaedition/InstanceController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Collections;
 import java.util.Map;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 @RestController
 @RequestMapping(path="/instance")
@@ -18,7 +21,7 @@ public class InstanceController {
 
 	@PostMapping(path="")
 	public Map<String, String> create(
-		@RequestParam java.sql.Timestamp datetime,
+		@RequestParam String datetime,
 		@RequestParam Float kp,
 		@RequestParam Float alt,
 		@RequestParam Float lat,
@@ -26,23 +29,29 @@ public class InstanceController {
 		@RequestParam Float lower,
 		@RequestParam Float upper,
 		@RequestParam Float step) {
-		Instance n = new Instance();
-		n.setOwner(1);
-		n.setName("New Instance");
-		n.setCreated(new java.sql.Timestamp(System.currentTimeMillis()));
-		n.setDatetime(datetime);
-		n.setModel("00");
-		n.setKp(kp);
-		n.setAlt(alt);
-		n.setLat(lat);
-		n.setLon(lon);
-		n.setVertical(new Float(.0));
-		n.setAzimutal(new Float(.0));
-		n.setLower(lower);
-		n.setUpper(upper);
-		n.setStep(step);
-		n.setFlightTime(new Float(.1));
-    instanceRepository.save(n);
-		return Collections.singletonMap("id", n.getId());
+		try {
+			Instance n = new Instance();
+			n.setOwner(1);
+			n.setName("New Instance");
+			n.setCreated(new java.sql.Timestamp(System.currentTimeMillis()));
+			DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss");
+			Date date = (Date) df.parse(datetime);
+			n.setDatetime(new java.sql.Timestamp(date.getTime()));
+			n.setModel("00");
+			n.setKp(kp);
+			n.setAlt(alt);
+			n.setLat(lat);
+			n.setLon(lon);
+			n.setVertical(new Float(.0));
+			n.setAzimutal(new Float(.0));
+			n.setLower(lower);
+			n.setUpper(upper);
+			n.setStep(step);
+			n.setFlightTime(new Float(.1));
+			instanceRepository.save(n);
+			return Collections.singletonMap("id", n.getId());
+		} catch(java.text.ParseException e) {
+			return Collections.singletonMap("error", "parsing");
+		}
 	}
 }
